2a55a25a44b36cd83666510d9921f03cbc35a664
Saumya khushlani <saumyakhushlani9@gmail.com>
2026-04-22 10:37:19 +0530

    Lab Evaluation

--- lab_evaluation.cpp
+++ lab_evaluation.cpp
@@ -106,7 +106,6 @@ int main() {
 #include <bits/stdc++.h>
 using namespace std;
 
-// Partition function
 int partition(vector<int> &arr, int low, int high) {
     int pivot = arr[low];   
     int i = low;
@@ -139,6 +138,113 @@ int main() {
     quickSort(arr, 0, arr.size() - 1);
 
     for (int x : arr) cout << x << " ";
+
+    return 0;
+}
+
+
+
+#include <iostream>
+#include <queue>
+#include <vector>
+using namespace std;
+
+int main() {
+    int p, q, r;
+    cin >> p >> q >> r;
+
+    int total = p + q + r;
+
+    if (max({p, q, r}) > (total + 1) / 2) {
+        cout << "Not Possible!" << endl;
+        return 0;
+    }
+
+    priority_queue<pair<int,char>> pq;
+    if (p > 0) pq.push({p, 'P'});
+    if (q > 0) pq.push({q, 'Q'});
+    if (r > 0) pq.push({r, 'R'});
+
+    string result = "";
+    char last = '#'; 
+
+    while (!pq.empty()) {
+        auto [cnt1, ball1] = pq.top(); pq.pop();
+
+        if (ball1 != last) {
+            result += ball1;
+            last = ball1;
+            if (cnt1 - 1 > 0) pq.push({cnt1 - 1, ball1});
+        } else {
+            if (pq.empty()) {
+                cout << "Not Possible!" << endl;
+                return 0;
+            }
+            auto [cnt2, ball2] = pq.top(); pq.pop();
+            result += ball2;
+            last = ball2;
+            if (cnt2 - 1 > 0) pq.push({cnt2 - 1, ball2});
+            pq.push({cnt1, ball1}); 
+        }
+    }
+
+    cout << "Arrangement: " << result << endl;
+    cout << "Length: " << result.size() << endl;
+    return 0;
+}
+
+
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+bool canPlace(vector<int>& pos, int k, int dist) {
+    int count = 1;
+    int last = pos[0];
+
+    for (int i = 1; i < pos.size(); i++) {
+        if (pos[i] - last >= dist) {
+            count++;
+            last = pos[i];
+        }
+        if (count >= k) return true;
+    }
+    return false;
+}
+
+int maxMinDistance(vector<int>& pos, int k) {
+    sort(pos.begin(), pos.end());
+
+    int low = 1;
+    int high = pos.back() - pos[0];
+    int ans = 0;
+
+    while (low <= high) {
+        int mid = (low + high) / 2;
+
+        if (canPlace(pos, k, mid)) {
+            ans = mid;
+            low = mid + 1;
+        } else {
+            high = mid - 1;
+        }
+    }
+    return ans;
+}
+
+int main() {
+    int n, k;
+    cin >> n;          
+    vector<int> pos(n);
+
+    for (int i = 0; i < n; i++) {
+        cin >> pos[i];
+    }
+
+    cin >> k;          
+
+    cout << maxMinDistance(pos, k) << endl;
 
     return 0;
 }
